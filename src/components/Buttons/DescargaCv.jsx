2edb93c358959e8c1f5205eb27e3196c4d1da670
KariRomero <karinaromero.1993@gmail.com>
2024-03-17 11:41:06 -0300

    borre png inutilizados, actualice ruta de DescargaCv

--- src/components/Buttons/DescargaCv.jsx
+++ src/components/Buttons/DescargaCv.jsx
@@ -5,7 +5,7 @@ const DescargaCv=({ selectedLanguage })=>{
     return(
         <button className="text-black font-medium rounded-lg text-base px-5 py-2.5 me-2 mb-4 mt-10 transition ease-in-out delay-150 bg-green hover:-translate-y-1 hover:scale-110 hover:bg-beige duration-300">
             <a 
-            href={selectedLanguage === 'Es' ? 'src/assets/CV-RomeroKarina.pdf' : 'src/assets/Resume-RomeroKarina.pdf'} 
+            href={selectedLanguage === 'Es' ? 'assets/CV-RomeroKarina.pdf' : 'assets/Resume-RomeroKarina.pdf'} 
             download={selectedLanguage === 'Es' ? 'Cv-RomeroKarina' : 'Resume-RomeroKarina'}>
                 {
                     selectedLanguage === 'Es' ? 'Descarga mi Cv ' : 'Download my resume '
